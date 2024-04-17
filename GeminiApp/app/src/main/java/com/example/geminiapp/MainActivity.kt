890518f83826000e64d0366be0dc9d5859117f7a
Akash <akashkobal02@gmail.com>
2024-04-17 09:52:23 +0530

    Gemini App update

---
+++ GeminiApp/app/src/main/java/com/example/geminiapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.geminiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.geminiapp.ui.theme.GeminiAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GeminiAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
